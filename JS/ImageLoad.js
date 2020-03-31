a459a065b727811ddebe7409bb1952a0f8342199
Wrafttex <nkure19@studen.aau.dk>
2020-03-31 14:43:17 +0200

    Function calls moved to HTML

--- JS/ImageLoad.js
+++ JS/ImageLoad.js
@@ -1,29 +1,10 @@
-// Input path 
-const images = ["../Source-Material/TestAlbum/Test1.jpeg",
-    "../Source-Material/TestAlbum/Test2.jpeg",
-    "../Source-Material/TestAlbum/Test3.jpeg",
-    "../Source-Material/TestAlbum/Test4.jpeg",
-    "../Source-Material/TestAlbum/Test5.jpeg",
-];
-
-const albums = ["Test Album", "Brasilien Tur", "Sydpolen Tur", "USA tur"];
-
-// Imput links
-// const images = [
-//     "https://cdn.pixabay.com/photo/2014/03/03/16/12/cinque-terre-279013_960_720.jpg",
-//     "https://cdn.pixabay.com/photo/2020/03/19/16/49/genoa-4948029_960_720.jpg",
-//     "https://cdn.pixabay.com/photo/2020/03/22/10/26/namibia-4956610_960_720.jpg",
-//     "https://cdn.pixabay.com/photo/2015/05/31/12/14/iphone-791450_960_720.jpg",
-//     "https://cdn.pixabay.com/photo/2020/03/21/19/27/sea-4955005_960_720.jpg",
-// ];
-
 // Creating image elements
-function loadImage() {
-    for (let index = 0; index < images.length; index++) {
+function loadImage(albumName) {
+    for (let index = 0; index < albumName.length; index++) {
         let image = new Image();
         image.id = "image" + index;
-        image.src = images[index];
-        image.alt = 'Failed to load ' + images[index];
+        image.src = albumName[index];
+        image.alt = 'Failed to load ' + albumName[index];
         image.height = "200";
         image.width = "400";
         image.style.padding = "5px";
@@ -37,7 +18,7 @@ function setUpAlbum(albumName) {
     document.title = albumName;
 }
 
-function updateDropdown() {
+function updateDropdown(albums) {
     for (let index = 0; index < albums.length; index++) {
         let album = document.getElementById("dropdown");
         let list = document.createElement("li");
@@ -46,7 +27,7 @@ function updateDropdown() {
     }
 }
 
-function addAnchor() {
+function addAnchor(albums) {
     for (let index = 0; index < albums.length; index++) {
         let list = document.getElementById("list" + index);
         let anchor = document.createElement("a");
@@ -56,8 +37,3 @@ function addAnchor() {
         list.appendChild(anchor);
     }
 }
-
-setUpAlbum(albums[0]);
-updateDropdown();
-addAnchor();
-loadImage();
---
+++ JS/pathfile.js
@@ -0,0 +1,9 @@
+const TestAlbumImages = ["../Source-Material/TestAlbum/Test1.jpeg",
+    "../Source-Material/TestAlbum/Test2.jpeg",
+    "../Source-Material/TestAlbum/Test3.jpeg",
+    "../Source-Material/TestAlbum/Test4.jpeg",
+    "../Source-Material/TestAlbum/Test5.jpeg",
+];
+
+// Input albums with name
+const albums = ["Test Album", "Brasilien Tur", "Sydpolen Tur", "USA tur"];

e053137666d9d3651954f520cc4b8709f719ba38
Wrafttex <nkure19@studen.aau.dk>
2020-04-01 22:02:59 +0200

    big image

--- JS/ImageLoad.js
+++ JS/ImageLoad.js
@@ -3,11 +3,12 @@ function loadImage(albumName) {
     for (let index = 0; index < albumName.length; index++) {
         let image = new Image();
         image.id = "image" + index;
+        image.classList.add("LoadedImage");
         image.src = albumName[index];
         image.alt = 'Failed to load ' + albumName[index];
         image.height = "200";
-        image.width = "400";
-        image.style.padding = "5px";
+        image.width = "300";
+        image.style.padding = "0 0 0 1em";
         document.getElementById('body').appendChild(image);
     }
 }

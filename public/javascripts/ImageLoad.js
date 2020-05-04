a52e4e84c3ecaf4a8835c65257f51c1d554c029a
Wrafttex <nkure19@studen.aau.dk>
2020-05-04 21:20:07 +0200

    Nu er magins shit os med

--- public/javascripts/ImageLoad.js
+++ public/javascripts/ImageLoad.js
@@ -10,7 +10,7 @@ async function apiCall(data) {
     return respons.body;
 }
 
-let retrunValue;
+let returnValue;
 
 async function getApiTable(tableAlbumName) {
     let result;
@@ -18,12 +18,14 @@ async function getApiTable(tableAlbumName) {
         job: "getData",
         albumName: tableAlbumName
     });
-    retrunValue = JSON.parse(result.data);
-    console.log(retrunValue);
+    returnValue = JSON.parse(result.data);
+    console.log(returnValue);
 
 
-    createDiv(retrunValue, "albumImages");
-    createImage(retrunValue, "albumImages");
+    createDiv(returnValue, "albumImages");
+    createImage(returnValue, "albumImages");
+    createModalDiv(returnValue, "popUpcontent");
+    createModalImage(returnValue);
 }
 
 
@@ -82,7 +84,8 @@ function createImage(arrayName, destination) {
         //image.alt = "Failed to load " + arrayName[index];
         //console.log("Image created");
         image.addEventListener("click", function() {
-            imagePopup(image);
+            openModal();
+            currentSlides(parseInt(image.id, 10));
         });
         //console.log("Sending image");
         appendImage(image, destination, index);
@@ -136,24 +139,6 @@ function setUpAlbum(albumName) {
     //console.log("Album setup")
 }
 
-
-// Enable the popup windown and adding the images + info
-function imagePopup(image) {
-    let modal = document.getElementById("myModal");
-    let modalImg = document.getElementById("img01");
-    let captionText = document.getElementById("caption");
-    let name = image.src.replace(/^.*[\\\/]/, '');
-    modal.style.display = "block";
-    modalImg.src = image.src;
-    captionText.innerHTML = name.split('.')[0];
-}
-
-// Closing the popup
-function closePopup() {
-    let popup = document.getElementById("myModal");
-    popup.style.display = "none";
-}
-
 let amountOfImages = 0;
 
 
@@ -180,7 +165,7 @@ function savePlacement() {
 }
 
 function sendToServer(name, base64) {
-    if (!retrunValue.includes(name)) {
+    if (!returnValue.includes(name)) {
         console.log("Sending images to server");
         apiCall({
                 job: "imageUploade",
@@ -240,4 +225,246 @@ function createNewImageDivs(extraSpcareArray) {
         appendDiv(imageDiv, rowID + "albumImages");
         idIndex++;
     }
+}
+
+// Creating div for modal/pop up
+function createModalDiv(arrayname, destination) {
+    let imageModalID = 0
+
+    if (localStorage.getItem('modalImageID') != undefined) {
+        imageModalID = localStorage.getItem('modalImageID');
+    }
+
+    for (let index = 0; index < arrayname.length; index++) {
+        let modalImageDiv = document.createElement("div");
+        modalImageDiv.id = "modalImage" + imageModalID;
+        modalImageDiv.className = "modalSlide";
+        modalImageDiv.style.display = "none";
+        appendModalDiv(modalImageDiv, destination);
+        imageModalID++;
+
+    }
+}
+
+
+function createModalImage(arrayName) {
+    let modalImageID = 0;
+
+    if (localStorage.getItem('modalImageID') != undefined) {
+        modalImageID = localStorage.getItem('modalImageID');
+    }
+
+    for (let index = 0; index < arrayName.length; index++) {
+        let image = new Image();
+        image.id = "modal" + modalImageID;
+        image.src = "/images/" + arrayName[index];
+        if (fileNames != undefined) {
+            image.name = fileNames[index];
+            console.log(image.name);
+        }
+        //image.alt = "Failed to load " + arrayName[index];
+        image.style.width = "auto";
+        //console.log("Image created");
+        //console.log("Sending image");
+        appendModalImage(image, modalImageID, "modalImage");
+        modalCaption(image, modalImageID)
+
+        modalImageID++;
+
+    }
+    localStorage.setItem('modalImageID', modalImageID);
+}
+
+function modalCaption(image, modalImageID) {
+    let divwrapper = document.createElement("div")
+    let buttonedit = document.createElement("button")
+    let buttonSave = document.createElement("button")
+    divwrapper.id = "divModalWrapper" + modalImageID;
+    divwrapper.className = "captionTable";
+    appendModalImage(divwrapper, modalImageID, "modalImage")
+
+    buttonedit.textContent = "Edit metadata"
+    buttonedit.className = "editcaption"
+    buttonedit.addEventListener("click", function() {
+        editableElement(divwrapper.id)
+
+    });
+    buttonSave.textContent = "Save Button"
+    buttonSave.className = "editcaption"
+    buttonSave.addEventListener("click", function() {
+        saveMetadata(divwrapper.id, modalImageID)
+    });
+
+    appendModalImage(buttonedit, modalImageID, "divModalWrapper")
+    appendModalImage(buttonSave, modalImageID, "divModalWrapper")
+
+    let exifdata
+    if (image.src.match('data:image/jpeg*')) {
+        exifdata = piexif.load(image.src);
+        // console.log(exifdata);
+    }
+
+
+    let metadataIntake = [269, 270, 315, 37510]
+    let metaplace = ["0th", "0th", "0th", "Exif"]
+    var ifds = ["0th", "Exif", "GPS", "Interop", "1st"];
+
+    if (image.src.match('data:image/jpeg*')) {
+        // console.log(exifdata["0th"][269])
+        // console.log(exifdata["0th"][270])
+        // console.log(exifdata["0th"][315])
+        // console.log(exifdata["Exif"][37510])
+
+        for (index = 0; index < metadataIntake.length; index++) {
+            let divmetadata = document.createElement("div")
+            let headmetadata = document.createElement("h3")
+            let pmetadata = document.createElement("p")
+            pmetadata.textContent = " "
+            divmetadata.class = "metadata";
+            divmetadata.id = piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"] + modalImageID
+                // console.log(piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"])
+            appendModalImage(divmetadata, modalImageID, "divModalWrapper");
+
+            headmetadata.textContent = piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"]
+            headmetadata.className = "headmetadata"
+            appendModalImage(headmetadata, modalImageID, piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"]);
+
+            pmetadata.className = "pmetadata"
+            pmetadata.contentEditable = false
+            if (piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"] == "DocumentName" && exifdata[metaplace[index]][metadataIntake[index]] === undefined) {
+                pmetadata.textContent = image.name.substr(0, image.name.lastIndexOf('.'));
+            } else {
+                pmetadata.textContent = exifdata[metaplace[index]][metadataIntake[index]];
+            }
+            appendModalImage(pmetadata, modalImageID, piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"])
+
+        }
+
+    }
+
+}
+
+function editableElement(divID) {
+    let captionedit = document.getElementById(divID).getElementsByClassName("pmetadata")
+    console.log(captionedit[0].textContent)
+    console.log(captionedit)
+    console.log(captionedit[0])
+    for (let index = 0; index < captionedit.length; index++) {
+
+        captionedit[index].contentEditable = (captionedit[index].contentEditable === "false") ? true : false;
+        // if((captionedit[index].contentEditable) === "true"){
+        //     captionedit[index].contentEditable = false
+        //     console.log("test")
+        // }
+        // else {
+        //     captionedit[index].contentEditable = true
+        // }
+
+    }
+
+    //document.getElementById("caption").contentEditable = true;
+
+}
+
+function saveMetadata(divID, numberID) {
+
+
+
+
+    let captionhead = document.getElementById(divID).getElementsByClassName("headmetadata") //meh
+    let captionedit = document.getElementById(divID).getElementsByClassName("pmetadata")
+    let imageModal = document.getElementById("modal" + numberID)
+    let imageNormal = document.getElementById(numberID)
+
+
+    let metadataIntake = [269, 270, 315, 37510]
+    let metaplace = ["0th", "0th", "0th", "Exif"]
+    let zerothIfd = {};
+    let exifIfd = {};
+    for (let index = 0; index < captionedit.length; index++) {
+        if (piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"] == "DocumentName") {
+            zerothIfd[piexif.ImageIFD.DocumentName] = captionedit[index].textContent
+        } else if (piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"] == "ImageDescription") {
+            zerothIfd[piexif.ImageIFD.ImageDescription] = captionedit[index].textContent
+        } else if (piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"] == "Artist") {
+            zerothIfd[piexif.ImageIFD.Artist] = captionedit[index].textContent
+        } else if (piexif.TAGS[metaplace[index]][metadataIntake[index]]["name"] == "UserComment") {
+            exifIfd[piexif.ExifIFD.UserComment] = captionedit[index].textContent
+        }
+    }
+    let exifArray = { "0th": zerothIfd, "Exif": exifIfd };
+    let exifBinary = piexif.dump(exifArray)
+    let newBase64JPG = piexif.insert(exifBinary, imageModal.src)
+    imageModal.src = newBase64JPG
+    imageNormal.src = newBase64JPG
+
+
+
+
+
+}
+
+
+function appendModalDiv(div, ID) {
+    let destinationDiv = document.getElementById(ID);
+    destinationDiv.appendChild(div);
+}
+
+
+function appendModalImage(image, index, divplace) {
+    let destinationImg = document.getElementById(divplace + index);
+    destinationImg.appendChild(image);
+    //  console.log("Image appened");
+
+}
+
+function openModal() {
+
+    document.getElementById("flexPopup").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("flexPopup").style.display = "none";
+}
+
+function currentSlides(ID) {
+    showSlides(slideIndex = ID);
+
+}
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// remember that slide lenght is one number higher than our index, as index starts at zero and lenght starts at one
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("modalSlide");
+    // console.log(n);
+    // console.log(slides.length);
+
+
+
+    if (n > slides.length - 1) {
+        slideIndex = 0
+    }
+
+    if (n < 0) {
+        slideIndex = slides.length - 1
+    }
+    // console.log(slideIndex);
+
+    ;
+    // console.log(document.getElementById(slideIndex).name);
+    let captionText = document.getElementById("caption");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+
+    }
+    slides[slideIndex].style.display = "block";
+
+
+
 }
